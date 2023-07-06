bf836439d5026125790b4d50ba38b474b56ddae6
Kartik Patel <kartikpatel0170@gmail.com>
2023-07-06 14:20:57 -0600

    login api integration

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,70 +1,202 @@
-import { View, Text, TouchableOpacity, Image, TextInput } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector, useDispatch } from "react-redux";
 import { Ionicons } from "@expo/vector-icons";
-// import { themeColors } from '../theme'
-import { useNavigation } from "@react-navigation/native";
+import { reset, login } from "../../features/auth/AuthSlice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import API from "../../app/api";
 
-export function Login() {
-  const navigation = useNavigation();
+export function Login({ navigation }) {
+  const dispatch = useDispatch();
+  const { user, isLoading, isLoggedIn } = useSelector((state) => state.auth);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const [visiblePassword, setVisiblePassword] = useState(true);
+
+  useEffect(() => {
+    if (isLoggedIn || user) {
+      if (isLoggedIn) {
+        console.log("user is logged in");
+        console.log(user);
+      }
+    }
+  }, [user, isLoggedIn, dispatch]);
+
+  const formInputHandler = (name, value) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await API.post("auth/login", userData);
+      if (response.data.status === true) {
+        await AsyncStorage.setItem("token", response.data.data.token);
+        await AsyncStorage.setItem("user", JSON.stringify(response.data.data));
+        await AsyncStorage.setItem("isLoggedIn", "true");
+
+        const data = {
+          user: response.data.data,
+          token: response.data.data.token,
+        };
+
+        dispatch(login(data));
+        alert("You have successfully logged in!");
+      }
+      
+    } catch (error) {
+      console.log(error);
+      if (error) {
+        alert(error);
+      } else {
+        alert("Please contact the administrator at katlin@farmsimple.ca!");
+      }
+    }
+  };
+
   return (
-    <View className="flex-1 bg-white" style={{ backgroundColor: "white" }}>
-      <SafeAreaView className="flex ">
-        <View className="flex-row justify-start">
-          <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            className=" p-2 rounded-tr-2xl rounded-bl-2xl ml-4"
+    <ScrollView>
+      <View style={{ flex: 1, backgroundColor: "smokewhite" }}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={{ flexDirection: "row", justifyContent: "start" }}>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{ padding: 10, borderRadius: 20, marginLeft: 8 }}
+            >
+              <Ionicons name="arrow-back-outline" size={20} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <Image
+              source={require("../../assets/favicon.png")}
+              style={{ width: 352, height: 132 }}
+            />
+          </View>
+        </SafeAreaView>
+        {isLoading && (
+          <ActivityIndicator size="large" color="your-loading-color" />
+        )}
+
+        <View style={{ padding: 16 }}>
+          <Text style={{ color: "gray", fontWeight: "bold", marginLeft: 12 }}>
+            Email Address
+          </Text>
+          <TextInput
+            style={{
+              padding: 16,
+              backgroundColor: "white",
+              color: "black",
+              borderRadius: 20,
+              marginBottom: 8,
+            }}
+            onChangeText={(text) => formInputHandler("email", text)}
+            value={email}
+            placeholder="example@email.com"
+            keyboardType="email-address"
+            editable={true}
+          />
+          <Text style={{ color: "gray", fontWeight: "bold", marginLeft: 12 }}>
+            Password
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              backgroundColor: "white",
+              borderRadius: 20,
+              marginBottom: 8,
+            }}
           >
-            <Ionicons name='arrow-back-outline' size="20" color="black" />
-          </TouchableOpacity>
+            <TextInput
+              style={{
+                padding: 16,
+                backgroundColor: "white",
+                color: "black",
+                borderRadius: 20,
+                flexGrow: 1,
+              }}
+              onChangeText={(text) => formInputHandler("password", text)}
+              value={password}
+              placeholder="********"
+              editable={true}
+              secureTextEntry={visiblePassword}
+            />
+            <TouchableOpacity
+              onPress={() => setVisiblePassword(!visiblePassword)}
+              style={{ padding: 10 }}
+            >
+              <Ionicons
+                name={visiblePassword ? "eye-outline" : "eye-off-outline"}
+                size={20}
+                color="gray"
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-        <View className="flex-row justify-center">
-          <Image
-            source={require("../../assets/favicon.png")}
-            style={{ width: 352, height: 132 }}
-          />
-        </View>
-      </SafeAreaView>
-      <View
-        style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
-        className="flex-1 bg-white px-8 pt-8"
-      >
-        <View className="form space-y-2">
-          <Text className="text-gray-700 ml-4">Email Address</Text>
-          <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-            placeholder="email"
-            value="john@gmail.com"
-          />
-          <Text className="text-gray-700 ml-4">Password</Text>
-          <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
-            secureTextEntry
-            placeholder="password"
-            value="test12345"
-          />
-          <TouchableOpacity className="flex items-end">
-            <Text className="text-gray-700 mb-5">Forgot Password?</Text>
-          </TouchableOpacity>
-          <TouchableOpacity className="py-3 bg-[#0d8900] rounded-xl">
-            <Text className="text-xl font-bold text-center text-gray-200">
-              Login
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <Text className="text-xl text-gray-700 font-bold text-center py-5">
+
+        <TouchableOpacity
+          style={{
+            padding: 16,
+            backgroundColor: "#0d8900",
+            borderRadius: 20,
+            margin: 16,
+          }}
+          onPress={loginFormHandler}
+        >
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 20,
+            color: "gray",
+            fontWeight: "bold",
+            textAlign: "center",
+            paddingTop: 40,
+          }}
+        >
           Or
         </Text>
 
-        <View className="flex-row justify-center mt-7">
-          <Text className="text-gray-500 font-semibold">
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <Text style={{ color: "gray", fontWeight: "bold" }}>
             Don't have an account?
           </Text>
           <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-            <Text className="font-bold text-green-900"> Sign Up</Text>
+            <Text style={{ fontWeight: "bold", color: "green" }}>Sign Up</Text>
           </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
