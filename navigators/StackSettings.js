02e3f0bbdef1666be009878dd334f2b40bd0cbf8
Kartik Patel <kartikpatel0170@gmail.com>
2023-07-06 14:16:22 -0600

    added logout function

--- navigators/StackSettings.js
+++ navigators/StackSettings.js
@@ -1,27 +1,43 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Button } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { reset } from '../features/auth/AuthSlice';
 
 // screens
-import { Profile, Tasks } from '../screens';
 import ProfileScreen from '../screens/ProfileScreen';
 import NavigationBack from '../components/NavigationBack';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-const SettingsStack = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Settings" component={Tasks} />
-  </Stack.Navigator>
-);
+// Logout button component
+const LogoutButton = ({ onPress }) => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(reset()); // Dispatch the "reset" action to reset the state
+    onPress(); // Call the onPress function to navigate to the login screen
+  };
+
+  return (
+    <Button title="Logout" onPress={handleLogout} />
+  );
+};
+
 
 const MainNavigator = () => (
-  <Drawer.Navigator drawerPosition="right">
-    <Drawer.Screen name="ProfileScreen" component={ProfileScreen}  options={{
-        headerLeft: () => <NavigationBack />}} />
-    <Drawer.Screen name="Settings" component={SettingsStack} />
-  </Drawer.Navigator>
+  <Stack.Navigator>
+    <Stack.Screen
+      name="ProfileScreen"
+      component={ProfileScreen}
+      options={({ navigation }) => ({
+        headerLeft: () => <NavigationBack />,
+        headerRight: () => <LogoutButton onPress={() => navigation.navigate('Login')} />,
+      })}
+    />
+  </Stack.Navigator>
 );
 
 export default MainNavigator;
