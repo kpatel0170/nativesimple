2660d13d42a3dcc9683858ccbc05a03bcf6707df
Kartik Patel <kartikpatel0170@gmail.com>
2023-07-10 14:07:29 -0600

    updated api url + added middleware

--- app/api.js
+++ app/api.js
@@ -1,7 +1,24 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API = axios.create({
-  baseURL: "https://try-back.onrender.com/api/",
-});
+  baseURL: "https://app.farmsimple.ca/api/",
+})
+
+API.interceptors.request.use(
+  async (config) => {
+      const token = await AsyncStorage.getItem("token")
+      if(token){
+          config.headers.Authorization = `Bearer ${token}`  
+          //get authorization header
+      }
+      return config
+  }, //any function we make before a request
+  (err) => {
+      return Promise.reject(err)
+  }
+
+)
+
 
 export default API;
